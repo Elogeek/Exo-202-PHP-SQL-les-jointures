11531a70f1493558d85d8139ecbdb9d7d094aae8
elogeek <elodiechristin@gmail.com>
2021-03-05 19:02:28 +0100

    exo les jointures

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-
+include './DB/DB.php';
+$db = DB::getInstance();
 /**
  * 1. Commencez par importer le script SQL disponible dans le dossier SQL.
  * 2. Connectez vous à la base de données blog.
@@ -17,57 +18,74 @@
  */
 
 // TODO Votre code ici.
-$server = 'localhost';
-$user = 'root';
-$password = 'dev';
-$bdd = 'blog';
+$db = DB::getInstance();
 
-try {
-    $connect =new PDO("mysql:host=$server;dbname=$bdd;charset=utf8", $user, $password);
-    $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $connect->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-    $sql = $connect->prepare("SELECT Article.title, Article.content, 'Categorie.name'
-                                FROM Article
-                                    INNER JOIN Categorie C on Article.category_fk = C.id
-                            ");
+$search = $db->prepare("
+                                SELECT 'Article.id', 'Article.title', 'Article.content', 'Categorie.name'
+                                FROM article
+                                INNER JOIN categorie on 'article.category_fk'= 'categorie.id'
+");
 
-    $sql->execute();
-    echo"<pre>";
-    print_r($sql->fetchAll());
-    echo"</prev";
+$state = $search->execute();
+
+if ($state) {
+    foreach ($search->fetchAll() as $item) {
+        echo "<pre>";
+        print_r($item);
+        echo "</pre>";
+    }
+}
 
     /**
      * 4. Réalisez la même chose que le point 3 en utilisant un maximum d'alias.
      */
 
 // TODO Votre code ici.
-    $sql = $connect->prepare("SELECT ar.title, ar.content, C.name
-                                FROM Article AS ar 
-                                INNER JOIN Categorie C on ar.category_fk = C.id");
+$db = DB::getInstance();
 
+$search = $db->prepare("
+                                SELECT 'ar.id as i', 'ar.title as t', 'ar.content as c', 'ca.name as category'
+                                FROM article as ar
+                                INNER JOIN categorie as ca on 'ar.category_fk' = 'ca.id'
+");
+
+$state = $search->execute();
+
+if ($state) {
+    foreach ($search->fetchAll() as $item) {
+        echo "<pre>";
+        print_r($item);
+        echo "</pre>";
+    }
+}
 
     /**
-    * 5. Ajoutez un utilisateur dans la table utilisateur.
- *    Ajoutez des commentaires et liez un utilisateur au commentaire.
- *    Avec un LEFT JOIN, affichez tous les commentaires et liez le nom et le prénom de l'utilisateur ayant écris le comentaire.
- */
+     * 5. Ajoutez un utilisateur dans la table utilisateur.
+     *    Ajoutez des commentaires et liez un utilisateur au commentaire.
+     *    Avec un LEFT JOIN, affichez tous les commentaires et liez le nom et le prénom de l'utilisateur ayant écris le comentaire.
+     */
 
 // TODO Votre code ici.
-$sql = $connect->prepare("INSERT INTO Utilisateur(firstName, lastName, mail, password) 
-                                VALUES('bulle', 'bubulle','bulle@gmail.com','azerty')");
+$db = DB::getInstance();
+$search = $db->prepare("
+                                SELECT 'co.content', 'ut.firstname', 'ut.lastname'
+                                FROM commentaire as co
+                                LEFT JOIN utilisateur as ut on 'co.user_fk' = 'ut.id'
+");
 
-$sql = $connect->prepare("INSERT INTO Commentaire(content, user_fk, article_fk) 
-                                VALUES ('un joli texte sql', '3', '2')");
+$state = $search->execute();
 
-$sqlRequest = $connect->prepare("SELECT Article.title, 'Auteur.fristName', 'Auteur.lastName', 'Categorie.name'
-                                       FROM Article
-                                           LEFT JOIN Auteur A on A.id = Article.author_fk
-                                           LEFT JOIN Categorie C on C.id = Article.category_fk
-                                 ");
-
+if ($state) {
+    foreach ($search->fetchAll() as $item) {
+        echo "<pre>";
+        print_r($item);
+        echo "</pre>";
+    }
 }
-catch(PDOException $exeption) {
-    echo 'error'. $exeption->getMessage();
-}
+$search = $db->prepare("
+                                SELECT 'co.content', 'ut.firstname', 'ut.lastname'
+                                FROM commentaire as co
+                                LEFT JOIN utilisateur as ut on 'co.user_fk' = 'ut.id'
+");
 
